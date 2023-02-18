686cd754490f32a18af938dfb052bc8e08c47455
Vinh Thuy Tran <vinhthuy.tran@zuhlke.com>
2023-02-18 23:32:37 +0700

    initialize

---
+++ src/main/java/com/zulhke/zuello/ZuelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.zulhke.zuello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuelloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuelloApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zulhke/zuello/ZuelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zulhke.zuello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuelloApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
