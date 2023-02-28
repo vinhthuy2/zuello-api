c5374da9999468c7818777c5c4ceb6570d1650a7
vinhthuy2 <34227790+vinhthuy2@users.noreply.github.com>
2023-02-28 16:54:36 +0700

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,8 +3,8 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://test-role-webappdeploymentbucket-1tswswm5x94a/SpringBootZuelloApi.war /usr/local/tomcat9/webapps/SpringBootZuelloApi.war
+aws s3 cp s3://test-role-webappdeploymentbucket-1tswswm5x94a/SpringBootZuelloApi.war /usr/local/tomcat10/webapps/SpringBootZuelloApi.war
 
 
 # Ensure the ownership permissions are correct.
-chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+chown -R tomcat:tomcat /usr/local/tomcat10/webapps
